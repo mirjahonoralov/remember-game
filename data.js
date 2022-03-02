3bae16abc2fd3f66d66c13942974131a114437bc
mirjahon <mirjahonoralov@gmail.com>
2022-03-02 19:03:47 +0500

    new-game

---
+++ data.js
@@ -0,0 +1,50 @@
+export let imgsData = [
+  {
+    name: "cat",
+    img: "./images/cat.jpg",
+  },
+  {
+    name: "cat",
+    img: "./images/cat.jpg",
+  },
+  {
+    name: "leopard",
+    img: "./images/leopard.jpg",
+  },
+  {
+    name: "leopard",
+    img: "./images/leopard.jpg",
+  },
+  {
+    name: "dog",
+    img: "./images/dog.jpg",
+  },
+  {
+    name: "dog",
+    img: "./images/dog.jpg",
+  },
+  {
+    name: "graff",
+    img: "./images/graff.jpg",
+  },
+  {
+    name: "graff",
+    img: "./images/graff.jpg",
+  },
+  {
+    name: "monkey",
+    img: "./images/monkey.jpg",
+  },
+  {
+    name: "monkey",
+    img: "./images/monkey.jpg",
+  },
+  {
+    name: "whiteBear",
+    img: "./images/whiteBear.jpg",
+  },
+  {
+    name: "whiteBear",
+    img: "./images/whiteBear.jpg",
+  },
+];
